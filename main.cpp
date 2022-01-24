4599ee603ce3c6ad06a19b09cb567e0a1513dfb8
Raul <raulrufino0@gmail.com>
2022-01-24 09:59:20 -0300

    Create objects directory

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ int main()
   game.mem.SetInAddr(0x101, 0x50);
   game.mem.SetInAddr(0x102, 0x01);
   game.mem.SetInAddr(0x150, 0xff);
+  game.cpu.reg.PC = 0x100;
   cout << game.cpu.reg.PC << endl;
   game.cpu.execute_intruction(game.mem);
   cout << game.cpu.reg.PC << endl;
