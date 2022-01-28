86d516e65bd2553deaee2b04583d5d4ceb830097
Raul <raulrufino0@gmail.com>
2022-01-28 13:52:47 -0300

    Create arrays to represent the memory map

--- src/game/memory.h
+++ src/game/memory.h
@@ -13,7 +13,13 @@ class Memory {
   void SetInAddr(Address add, Byte byte_to_insert);
 
  private:
-  Byte cartridge[32 * KB];
+  Byte          cartridge_ROM[0x8000 - 0x0000] = {0x0};
+  Byte                   VRAM[0xA000 - 0x8000] = {0x0};
+  Byte          cartridge_RAM[0xC000 - 0xA000] = {0x0};
+  Byte                 WRAM_0[0xD000 - 0xC000] = {0x0};
+  Byte                 WRAM_N[0xE000 - 0xD000] = {0x0};
+  Byte                    OAM[0xFEA0 - 0xFE00] = {0x0};
+  Byte IO_REG_and_HRAM_and_IE[0xFFFF - 0xFF00] = {0x0};
 };
 
 }
