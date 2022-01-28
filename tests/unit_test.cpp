281c04005e4f861c44b3ec2b2bb9ab0be91097e8
Raul <raulrufino0@gmail.com>
2022-01-27 22:43:52 -0300

    add tests to LD_a16 instruction

--- tests/unit_test.cpp
+++ tests/unit_test.cpp
@@ -2,6 +2,7 @@
 #include <gtest/gtest.h>
 
 #include "../src/game/game.h"
+#include "../src/game/primitives.h"
 #include "../src/game/opcodes.h"
 
 TEST(Instructions, JP_a16_instruction) {
@@ -10,7 +11,7 @@ TEST(Instructions, JP_a16_instruction) {
   game.mem.SetInAddr(0x101, 0x50);
   game.mem.SetInAddr(0x102, 0x01);
   game.cpu.reg.PC = 0x100;
-  game.cpu.execute_intruction(game.mem);
+  game.cpu.execute_intruction(&game.mem);
 
   EXPECT_EQ(game.cpu.reg.PC, 0x150);
 }
@@ -22,9 +23,26 @@ TEST(Instructions, LD_d8_instruction) {
   game.mem.SetInAddr(0x101, to_load);
   game.cpu.reg.PC = 0x100;
   game.cpu.reg.A = 0x00;
-  game.cpu.execute_intruction(game.mem);
+  game.cpu.execute_intruction(&game.mem);
 
   EXPECT_EQ(game.cpu.reg.A, to_load);
   EXPECT_EQ(game.cpu.reg.PC, 0x102);
 }
+
+TEST(Instructions, LD_a16_instruction) {
+  gameboy::Console game;
+  Address address_to_store = 0x150;
+  game.mem.SetInAddr(0x100, LD_a16);
+  game.mem.SetInAddr(0x101, 0x50);
+  game.mem.SetInAddr(0x102, 0x01);
+  game.mem.SetInAddr(address_to_store, 0x0);
+  game.cpu.reg.PC = 0x100;
+  game.cpu.reg.A = 0x50;
+  game.cpu.execute_intruction(&game.mem);
+
+  EXPECT_EQ(game.cpu.reg.A, 0x50);
+  EXPECT_EQ(game.cpu.reg.PC, 0x103);
+  EXPECT_EQ(game.mem.GetInAddr(address_to_store), game.cpu.reg.A);
+}
+
 
