ca778d541301127e3e8525e42d5c37e88727af59
Raul <raulrufino0@gmail.com>
2022-01-30 14:07:28 -0300

    Add GTKMM library

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 #include <string>
+#include <gtkmm.h>
 #include "src/game/game.h"
 
 using namespace std;
 
-int main() {
-  gameboy::Console game;
-  return 0;
+
+
+int main(int argc, char *argv[])
+{
+  auto app =
+    Gtk::Application::create(argc, argv,
+      "org.gtkmm.examples.base");
+
+  Gtk::Window window;
+  window.set_default_size(200, 200);
+
+  return app->run(window);
 }
+
