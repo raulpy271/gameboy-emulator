d6fc9b55290ec43b496af9f087482917a5ccab26
Raul <raulrufino0@gmail.com>
2022-01-31 22:34:44 -0300

    Experiment using pixbuffer

--- main.cpp
+++ main.cpp
@@ -5,6 +5,24 @@
 
 using namespace std;
 
+Glib::RefPtr<Gdk::Pixbuf> create_pixbuf(int width, int height) {
+  guint8* array_pixel = new guint8[3 * width * height];
+  for (int i = 0; i < (3 * width * height) - 3; i += 3) {
+    array_pixel[i] = 255;
+    array_pixel[i + 1] = 255;
+    array_pixel[i + 2] = 255;
+  }
+  Glib::RefPtr<Gdk::Pixbuf> pixbuf = Gdk::Pixbuf::create_from_data(
+    array_pixel,
+    Gdk::COLORSPACE_RGB,
+    false,
+    8,
+    width,
+    height,
+    3 * height
+  );
+  return pixbuf;
+}
 
 
 int main(int argc, char *argv[])
@@ -15,6 +33,15 @@ int main(int argc, char *argv[])
 
   Gtk::Window window;
   window.set_default_size(200, 200);
+
+
+  Glib::RefPtr<Gdk::Pixbuf> pixbuf = create_pixbuf(100, 100);
+  Gtk::Image image(pixbuf);
+
+  window.add(image);
+
+  image.show();
+  window.show();
 
   return app->run(window);
 }
