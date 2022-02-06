443c1307d525ea200c5ccf46d0b721bbc49d787c
Raul <raulrufino0@gmail.com>
2022-02-06 16:42:35 -0300

    Add function to manipulate zero flag

--- src/utils/register_F_manipulate.cpp
+++ src/utils/register_F_manipulate.cpp
@@ -14,4 +14,15 @@ void set_carry_flag(Byte* F, bool value) {
   *F = (Byte) f_bitset.to_ulong();
 }
 
+bool zero_flag(Byte* F) {
+  std::bitset<8> f_bitset(*F);
+  return (bool) f_bitset[ZERO_FLAG_POSITION];
+}
+
+void set_zero_flag(Byte* F, bool value) {
+  std::bitset<8> f_bitset(*F);
+  f_bitset.set(ZERO_FLAG_POSITION, value);
+  *F = (Byte) f_bitset.to_ulong();
+}
+
 }
--- src/utils/register_F_manipulate.h
+++ src/utils/register_F_manipulate.h
@@ -7,12 +7,17 @@
 #include "../game/primitives.h"
 
 #define CARRY_FLAG_POSITION 4
+#define ZERO_FLAG_POSITION 7
 
 namespace utils {
 
 bool carry_flag(Byte* F);
 
 void set_carry_flag(Byte* F, bool value);
+
+bool zero_flag(Byte* F);
+
+void set_zero_flag(Byte* F, bool value);
 
 }
 
