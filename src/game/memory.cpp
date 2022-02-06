efa35dec42cc96ccb850cca39904e865e223d132
Raul <raulrufino0@gmail.com>
2022-02-06 11:18:05 -0300

    Add memory mapping to bytes between 0xffff - 0xff00

--- src/game/memory.cpp
+++ src/game/memory.cpp
@@ -19,6 +19,9 @@ MemorySegment Memory::choose_segment(Address add) {
   if (add < 0xE000) {
     return MemorySegment::WRAM_N;
   }
+  if (add >= 0xFF00 && add <= 0xFFFF) {
+    return MemorySegment::IO_REG_and_HRAM_and_IE;
+  }
   return MemorySegment::NO_SEGMENT;
 }
 
@@ -27,6 +30,8 @@ Byte Memory::GetInAddr(Address add) {
   {
   case MemorySegment::CARTRIDGE_ROM:
     return cartridge_ROM[add];
+  case MemorySegment::IO_REG_and_HRAM_and_IE:
+    return IO_REG_and_HRAM_and_IE[add - 0xFF00];
   case MemorySegment::NO_SEGMENT:
     return 0xff;
   default:
@@ -39,6 +44,10 @@ void Memory::SetInAddr(Address add, Byte byte_to_insert) {
   {
   case MemorySegment::CARTRIDGE_ROM:
     cartridge_ROM[add] = byte_to_insert;
+    return;
+  case MemorySegment::IO_REG_and_HRAM_and_IE:
+    IO_REG_and_HRAM_and_IE[add - 0xFF00] = byte_to_insert;
+    return;
   case MemorySegment::NO_SEGMENT:
     return;
   default:
