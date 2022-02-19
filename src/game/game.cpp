aa6dc2dd278479fa3b8858c0d10f5549035a0f1a
Raul <raulrufino0@gmail.com>
2022-02-19 19:46:32 -0300

    Define and initialize hardware registers

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -1,5 +1,6 @@
 
 #include "game.h"
+#include "hardware_registers.h"
 
 namespace gameboy {
 
@@ -10,6 +11,70 @@ Console::Console() {
 void Console::initialize_registers() {
   cpu.reg.PC = 0x0100;
   cpu.reg.SP = 0xfffe;
+  cpu.reg.A  = 0x01;
+  cpu.reg.F  = 0b11000000;
+  cpu.reg.B  = 0x0;
+  cpu.reg.C  = 0x13;
+  cpu.reg.D  = 0x0;
+  cpu.reg.E  = 0xD8;
+  cpu.reg.H  = 0x01;
+  cpu.reg.L  = 0x4D;
+
+  mem.SetInAddr(P1 , 0xCF);
+  mem.SetInAddr(SB , 0x00);
+  mem.SetInAddr(SC , 0x7E);
+  mem.SetInAddr(DIV , 0xAB);
+  mem.SetInAddr(TIMA , 0x00);
+  mem.SetInAddr(TMA , 0x00);
+  mem.SetInAddr(TAC , 0xF8);
+  mem.SetInAddr(IF , 0xE1);
+  mem.SetInAddr(NR10 , 0x80);
+  mem.SetInAddr(NR11 , 0xBF);
+  mem.SetInAddr(NR12 , 0xF3);
+  mem.SetInAddr(NR13 , 0xFF);
+  mem.SetInAddr(NR14 , 0xBF);
+  mem.SetInAddr(NR21 , 0x3F);
+  mem.SetInAddr(NR22 , 0x00);
+  mem.SetInAddr(NR23 , 0xFF);
+  mem.SetInAddr(NR24 , 0xBF);
+  mem.SetInAddr(NR30 , 0x7F);
+  mem.SetInAddr(NR31 , 0xFF);
+  mem.SetInAddr(NR32 , 0x9F);
+  mem.SetInAddr(NR33 , 0xFF);
+  mem.SetInAddr(NR34 , 0xBF);
+  mem.SetInAddr(NR41 , 0xFF);
+  mem.SetInAddr(NR42 , 0x00);
+  mem.SetInAddr(NR43 , 0x00);
+  mem.SetInAddr(NR44 , 0xBF);
+  mem.SetInAddr(NR50 , 0x77);
+  mem.SetInAddr(NR51 , 0xF3);
+  mem.SetInAddr(NR52 , 0xF1);
+  mem.SetInAddr(LCDC , 0x91);
+  mem.SetInAddr(STAT , 0x85);
+  mem.SetInAddr(SCY , 0x00);
+  mem.SetInAddr(SCX , 0x00);
+  mem.SetInAddr(LY , 0x00);
+  mem.SetInAddr(LYC , 0x00);
+  mem.SetInAddr(DMA , 0xFF);
+  mem.SetInAddr(BGP , 0xFC);
+  mem.SetInAddr(OBP0 , 0x0);
+  mem.SetInAddr(OBP1 , 0x0);
+  mem.SetInAddr(WY , 0x00);
+  mem.SetInAddr(WX , 0x00);
+  mem.SetInAddr(KEY1 , 0xFF);
+  mem.SetInAddr(VBK , 0xFF);
+  mem.SetInAddr(HDMA1 , 0xFF);
+  mem.SetInAddr(HDMA2 , 0xFF);
+  mem.SetInAddr(HDMA3 , 0xFF);
+  mem.SetInAddr(HDMA4 , 0xFF);
+  mem.SetInAddr(HDMA5 , 0xFF);
+  mem.SetInAddr(RP , 0xFF);
+  mem.SetInAddr(BCPS , 0xFF);
+  mem.SetInAddr(BCPD , 0xFF);
+  mem.SetInAddr(OCPS , 0xFF);
+  mem.SetInAddr(OCPD , 0xFF);
+  mem.SetInAddr(SVBK , 0xFF);
+  mem.SetInAddr(IE , 0x00);
 }
 
 void Console::run_a_instruction_cycle() {
---
+++ src/game/hardware_registers.h
@@ -0,0 +1,61 @@
+
+#ifndef GAME_HARDWARE_REGISTERS_H_
+#define GAME_HARDWARE_REGISTERS_H_
+
+#define P1     0xFF00
+#define SB     0xFF01
+#define SC     0xFF02
+#define DIV    0xFF04
+#define TIMA   0xFF05
+#define TMA    0xFF06
+#define TAC    0xFF07
+#define IF     0xFF0F
+#define NR10   0xFF10
+#define NR11   0xFF11
+#define NR12   0xFF12
+#define NR13   0xFF13
+#define NR14   0xFF14
+#define NR21   0xFF16
+#define NR22   0xFF17
+#define NR23   0xFF18
+#define NR24   0xFF19
+#define NR30   0xFF1A
+#define NR31   0xFF1B
+#define NR32   0xFF1C
+#define NR33   0xFF1D
+#define NR34   0xFF1E
+#define NR41   0xFF20
+#define NR42   0xFF21
+#define NR43   0xFF22
+#define NR44   0xFF23
+#define NR50   0xFF24
+#define NR51   0xFF25
+#define NR52   0xFF26
+#define LCDC   0xFF40
+#define STAT   0xFF41
+#define SCY    0xFF42
+#define SCX    0xFF43
+#define LY     0xFF44
+#define LYC    0xFF45
+#define DMA    0xFF46
+#define BGP    0xFF47
+#define OBP0   0xFF48
+#define OBP1   0xFF49
+#define WY     0xFF4A
+#define WX     0xFF4B
+#define KEY1   0xFF4D
+#define VBK    0xFF4F
+#define HDMA1  0xFF51
+#define HDMA2  0xFF52
+#define HDMA3  0xFF53
+#define HDMA4  0xFF54
+#define HDMA5  0xFF55
+#define RP     0xFF56
+#define BCPS   0xFF68
+#define BCPD   0xFF69
+#define OCPS   0xFF6A
+#define OCPD   0xFF6B
+#define SVBK   0xFF70
+#define IE     0xFFFF
+
+#endif  // GAME_HARDWARE_REGISTERS_H_
