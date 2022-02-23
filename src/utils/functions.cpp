fe6519914ce0befb90dbfb02e22450c94bcfc6ad
Raul <raulrufino0@gmail.com>
2022-02-23 09:47:25 -0300

    Add function to create two bytes from an anddress

--- src/utils/functions.cpp
+++ src/utils/functions.cpp
@@ -1,4 +1,7 @@
 
+#include <bitset>
+#include <math.h>
+
 #include "../game/primitives.h"
 #include "functions.h"
 
@@ -6,6 +9,26 @@ namespace utils {
 
 Address create_address_from_two_bytes(Byte higher_byte, Byte lower_byte) {
   return (higher_byte * 0x100) + lower_byte;
+}
+
+std::pair<Byte, Byte> create_two_bytes_from_address(Address addr) {
+  std::bitset<16> addr_bitset(addr);
+  std::pair<Byte, Byte> two_bytes;
+  Byte higher_byte = 0;
+  Byte lower_byte = 0;
+  for (int i = 0; i < 8; i++) {
+    if (addr_bitset[i]) {
+      lower_byte += pow(2, i);
+    }
+  }
+  for (int i = 0; i < 8; i++) {
+    if (addr_bitset[8 + i]) {
+      higher_byte += pow(2, i);
+    }
+  }
+  two_bytes.first = higher_byte;
+  two_bytes.second = lower_byte;
+  return two_bytes;
 }
 
 void increment_registers_pair(Byte* higher_byte_reg, Byte* lower_byte_reg) {
--- src/utils/functions.h
+++ src/utils/functions.h
@@ -1,9 +1,16 @@
 
+#ifndef UTILS_FUNCTIONS_H_
+#define UTILS_FUNCTIONS_H_
+
+#include <utility>
+
 #include "../game/primitives.h"
 
 namespace utils {
 
 Address create_address_from_two_bytes(Byte higher_byte, Byte lower_byte);
+
+std::pair<Byte, Byte> create_two_bytes_from_address(Address addr);
 
 void increment_registers_pair(Byte* higher_byte_reg, Byte* lower_byte_reg);
 
@@ -11,5 +18,6 @@ void decrement_registers_pair(Byte* higher_byte_reg, Byte* lower_byte_reg);
 
 unsigned int integer_division(unsigned int n, unsigned int d);
 
-
 }
+
+#endif  // UTILS_FUNCTIONS_H_
