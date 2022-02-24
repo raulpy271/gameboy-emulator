87f619669cf1657d44ab6b46081191ab2332f6b1
Raul <raulrufino0@gmail.com>
2022-02-24 08:22:34 -0300

    Add implementation of `RES 7 [HL]` Instruction

--- src/game/cpu/cpu_prefixed_instructions.cpp
+++ src/game/cpu/cpu_prefixed_instructions.cpp
@@ -1,4 +1,6 @@
 
+#include <bitset>
+
 #include "cpu.h"
 #include "cpu_utils.h"
 #include "../../utils/functions.h"
@@ -7,8 +9,14 @@
 namespace gameboy {
 
 void CPU::RES_7_aHL_Instruction(Memory* mem) {
-  // TODO
-  reg.PC += 3;
+  Address higher_byte = reg.H;
+  Address lower_byte = reg.L;
+  Address addr_to_reset = utils::create_address_from_two_bytes(higher_byte, lower_byte);
+  Byte value_to_reset = mem->GetInAddr(addr_to_reset);
+  std::bitset<8> value(value_to_reset);
+  value.reset(7);
+  mem->SetInAddr(addr_to_reset, value.to_ulong());
+  reg.PC += 2;
 }
 
 }
