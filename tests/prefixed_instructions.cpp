042a66edf6598878f901bbf56a698fb8f29c350a
Raul <raulrufino0@gmail.com>
2022-02-24 21:46:19 -0300

    Add tests to `SET 7 aHL` Instruction

--- tests/prefixed_instructions.cpp
+++ tests/prefixed_instructions.cpp
@@ -20,3 +20,18 @@ TEST(PrefixedInstructions, RES_7_aHL_instruction) {
   EXPECT_EQ(game.mem.GetInAddr(0x150), 0b01111111);
 }
 
+TEST(PrefixedInstructions, SET_7_aHL_instruction) {
+  gameboy::Console game;
+  game.initialize_registers();
+  game.mem.SetInAddr(0x100, OPCODE_PREFIX);
+  game.mem.SetInAddr(0x101, SET_7_aHL);
+  game.mem.SetInAddr(0x150, 0b01111111);
+  game.cpu.reg.H = 0x01;
+  game.cpu.reg.L = 0x50;
+
+  game.cpu.execute_intruction(&game.mem);
+
+  EXPECT_EQ(game.cpu.reg.PC, 0x102);
+  EXPECT_EQ(game.mem.GetInAddr(0x150), 0b11111111);
+}
+
