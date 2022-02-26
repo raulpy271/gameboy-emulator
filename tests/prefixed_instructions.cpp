b4592661aa276083e69e39757000a7c7aba0fd38
Raul <raulrufino0@gmail.com>
2022-02-25 22:20:51 -0300

    Add tests to `RES 1 aHL` Instruction

--- tests/prefixed_instructions.cpp
+++ tests/prefixed_instructions.cpp
@@ -20,6 +20,22 @@ TEST(PrefixedInstructions, RES_7_aHL_instruction) {
   EXPECT_EQ(game.mem.GetInAddr(0x150), 0b01111111);
 }
 
+TEST(PrefixedInstructions, RES_1_aHL_instruction) {
+  gameboy::Console game;
+  game.initialize_registers();
+  game.mem.SetInAddr(0x100, OPCODE_PREFIX);
+  game.mem.SetInAddr(0x101, RES_1_aHL);
+  game.mem.SetInAddr(0x150, 0b11111010);
+  game.cpu.reg.H = 0x01;
+  game.cpu.reg.L = 0x50;
+
+  game.cpu.execute_intruction(&game.mem);
+
+  EXPECT_EQ(game.cpu.reg.PC, 0x102);
+  EXPECT_EQ(game.mem.GetInAddr(0x150), 0b11111000);
+}
+
+
 TEST(PrefixedInstructions, SET_7_aHL_instruction) {
   gameboy::Console game;
   game.initialize_registers();
