43eedb68919f4b56d2ad60779fe9957d9a7c1ea0
Raul <raulrufino0@gmail.com>
2022-02-28 14:42:45 -0300

    Test the request interrupt method

--- tests/interrupts.cpp
+++ tests/interrupts.cpp
@@ -4,6 +4,8 @@
 #include "../src/game/game.h"
 #include "../src/game/primitives.h"
 #include "../src/game/cpu/opcodes.h"
+#include "../src/game/cpu/interrupts.h"
+#include "../src/game/hardware_registers.h"
 
 TEST(Interrupts, IE_instruction) {
   gameboy::Console game;
@@ -13,4 +15,15 @@ TEST(Interrupts, IE_instruction) {
 
   EXPECT_EQ(game.cpu.reg.PC, 0x101);
   EXPECT_EQ(game.cpu.GetIME(), true);
+}
+
+TEST(Interrupts, request_interrupts) {
+  gameboy::Console game;
+  game.initialize_registers();
+  game.mem.SetInAddr(rIF, 0x0);
+
+  game.cpu.RequestInterrupt(&game.mem, gameboy::InterruptFlag::VBLANK);
+  game.cpu.RequestInterrupt(&game.mem, gameboy::InterruptFlag::HiToLo);
+
+  EXPECT_EQ(game.mem.GetInAddr(rIF), 0b00010001);
 }
