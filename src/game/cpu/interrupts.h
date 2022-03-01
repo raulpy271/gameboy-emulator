78d7caaa1c4f393a925ef44806a75dd7a62deb20
Raul <raulrufino0@gmail.com>
2022-02-28 21:46:46 -0300

    Fix address of interrupt handlers(serial and timer)

--- src/game/cpu/interrupts.h
+++ src/game/cpu/interrupts.h
@@ -7,16 +7,16 @@ namespace gameboy {
 enum class InterruptVector {
   VBLANK = 0x40,
   LCDC   = 0x48,
-  SERIAL = 0x50,
-  TIMER  = 0x58,
+  TIMER  = 0x50,
+  SERIAL = 0x58,
   HiToLo = 0x60
 };
 
 enum class InterruptFlag {
   VBLANK      = 0,
   LCDC        = 1,
-  SERIAL      = 2,
-  TIMER       = 3,
+  TIMER       = 2,
+  SERIAL      = 3,
   HiToLo      = 4,
   NoInterrupt = 5
 };
