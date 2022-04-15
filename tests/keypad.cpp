7608585689a44fad66dbb5c45ec5b977aed51d04
Raul <raulrufino0@gmail.com>
2022-04-15 14:43:34 -0300

    Add tests to keypad generating interrupt

--- tests/keypad.cpp
+++ tests/keypad.cpp
@@ -1,70 +1,83 @@
 
 #include <gtest/gtest.h>
 
-#include "../src/game/memory.h"
+#include "../src/game/game.h"
 #include "../src/game/keypad.h"
 #include "../src/game/primitives.h"
 #include "../src/game/hardware_registers.h"
 
 TEST(Keypad, press_up) {
-  gameboy::Memory mem;
+  gameboy::Console game;
   Byte select_direction_bottons = 0b00101111;
   Byte up_clicked = 0b1011;
 
-  mem.SetInAddr(rP1, select_direction_bottons);
+  game.mem.SetInAddr(rP1, select_direction_bottons);
 
-  mem.HandleBottonEvent(gameboy::Botton::Up, gameboy::BottonEventType::PRESS);
+  game.HandleBottonEvent(gameboy::Botton::Up, gameboy::BottonEventType::PRESS);
 
-  EXPECT_EQ(mem.GetInAddr(rP1) & 0b1111, up_clicked);
+  EXPECT_EQ(game.mem.GetInAddr(rP1) & 0b1111, up_clicked);
 }
 
 TEST(Keypad, release_up) {
-  gameboy::Memory mem;
+  gameboy::Console game;
   Byte select_direction_bottons = 0b00101111;
   Byte up_clicked = 0b1011;
   Byte no_click = 0b1111;
 
-  mem.SetInAddr(rP1, select_direction_bottons);
+  game.mem.SetInAddr(rP1, select_direction_bottons);
 
-  mem.HandleBottonEvent(gameboy::Botton::Up, gameboy::BottonEventType::PRESS);
+  game.HandleBottonEvent(gameboy::Botton::Up, gameboy::BottonEventType::PRESS);
 
-  EXPECT_EQ(mem.GetInAddr(rP1) & 0b1111, up_clicked);
+  EXPECT_EQ(game.mem.GetInAddr(rP1) & 0b1111, up_clicked);
 
-  mem.HandleBottonEvent(gameboy::Botton::Up, gameboy::BottonEventType::RELEASE);
+  game.HandleBottonEvent(gameboy::Botton::Up, gameboy::BottonEventType::RELEASE);
 
-  EXPECT_EQ(mem.GetInAddr(rP1) & 0b1111, no_click);
+  EXPECT_EQ(game.mem.GetInAddr(rP1) & 0b1111, no_click);
 }
 
 TEST(Keypad, press_and_release_A) {
-  gameboy::Memory mem;
+  gameboy::Console game;
   Byte select_action_bottons = 0b00011111;
   Byte A_clicked = 0b1110;
   Byte no_click = 0b1111;
 
-  mem.SetInAddr(rP1, select_action_bottons);
+  game.mem.SetInAddr(rP1, select_action_bottons);
 
-  mem.HandleBottonEvent(gameboy::Botton::A, gameboy::BottonEventType::PRESS);
+  game.HandleBottonEvent(gameboy::Botton::A, gameboy::BottonEventType::PRESS);
 
-  EXPECT_EQ(mem.GetInAddr(rP1) & 0b1111, A_clicked);
+  EXPECT_EQ(game.mem.GetInAddr(rP1) & 0b1111, A_clicked);
 
-  mem.HandleBottonEvent(gameboy::Botton::A, gameboy::BottonEventType::RELEASE);
+  game.HandleBottonEvent(gameboy::Botton::A, gameboy::BottonEventType::RELEASE);
 
-  EXPECT_EQ(mem.GetInAddr(rP1) & 0b1111, no_click);
+  EXPECT_EQ(game.mem.GetInAddr(rP1) & 0b1111, no_click);
 }
 
 TEST(Keypad, press_down_and_B) {
-  gameboy::Memory mem;
+  gameboy::Console game;
   Byte select_direction_bottons = 0b00100000;
   Byte select_action_bottons = 0b00010000;
   Byte down_clicked = 0b0111;
   Byte B_clicked = 0b1101;
 
-  mem.HandleBottonEvent(gameboy::Botton::Down, gameboy::BottonEventType::PRESS);
-  mem.HandleBottonEvent(gameboy::Botton::B, gameboy::BottonEventType::PRESS);
+  game.HandleBottonEvent(gameboy::Botton::Down, gameboy::BottonEventType::PRESS);
+  game.HandleBottonEvent(gameboy::Botton::B, gameboy::BottonEventType::PRESS);
 
-  mem.SetInAddr(rP1, select_direction_bottons);
-  EXPECT_EQ(mem.GetInAddr(rP1) & 0b1111, down_clicked);
+  game.mem.SetInAddr(rP1, select_direction_bottons);
+  EXPECT_EQ(game.mem.GetInAddr(rP1) & 0b1111, down_clicked);
 
-  mem.SetInAddr(rP1, select_action_bottons);
-  EXPECT_EQ(mem.GetInAddr(rP1) & 0b1111, B_clicked);
+  game.mem.SetInAddr(rP1, select_action_bottons);
+  EXPECT_EQ(game.mem.GetInAddr(rP1) & 0b1111, B_clicked);
+}
+
+TEST(Keypad, press_start_and_generate_interruption) {
+  gameboy::Console game;
+  Byte select_action_bottons = 0b00010000;
+  Byte start_clicked = 0b0111;
+
+  game.mem.SetInAddr(rIF, 0);
+  game.HandleBottonEvent(gameboy::Botton::Start, gameboy::BottonEventType::PRESS);
+  game.mem.SetInAddr(rP1, select_action_bottons);
+
+  EXPECT_EQ(game.mem.GetInAddr(rP1) & 0b1111, start_clicked);
+  EXPECT_EQ(game.mem.GetInAddr(rIF), 0b00010000);
 }
