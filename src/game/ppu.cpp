a1abf2c4b51eda659a0b997d560092a10bdc2dda
Raul <raulrufino0@gmail.com>
2022-04-24 08:56:13 -0300

    Rewrite the PPU to draw image line by line

--- src/game/ppu.cpp
+++ src/game/ppu.cpp
@@ -1,4 +1,5 @@
 
+#include <iostream>
 #include <bitset>
 
 #include "ppu.h"
@@ -42,91 +43,86 @@ void PPU::ReadTileLine(ColorNumber* arr_to_store, Address tile_line_address, Byt
   }
 }
 
-void PPU::ScanLine(ColorNumber* arr_to_store_line, int background_Y_line, Byte palette) {
+void PPU::ScanLineBackground(ColorNumber* arr_to_store_line, int background_Y_line, Byte palette) {
+  ColorNumber background_line[256];
   unsigned int tile_data_position = 0;
   unsigned int tile_reference = 0;
   unsigned int tile_line = background_Y_line % 8;
   for (int tile_number = 0; tile_number < 32; tile_number++) {
     tile_reference = mem->GetInAddr(0x9800 + (32 * utils::integer_division(background_Y_line, 8)) + tile_number);
     tile_data_position = 0x8000 + (tile_reference * 16) + (2*tile_line);
-    ReadTileLine(arr_to_store_line + (tile_number * 8), tile_data_position, palette);
+    ReadTileLine((&(background_line[0])) + (tile_number * 8), tile_data_position, palette);
   }
-}
-
-void PPU::DrawSingleSprite(int y_screen, int x_screen, Byte tile_number, Byte palette) {
-  int x, y;
-  int tile_line_address;
-  ColorNumber tile_line[8];
-  int tile_line_number = 0;
-  int tile_column_number = 0;
-  for (y = y_screen; y < (y_screen + 8); y++, tile_line_number++) {
-    if (y < 0 || y >= SCREEN_Y_SIZE) {
-      continue;
-    }
-    tile_line_address = _VRAM8000 + (tile_number * 16) + (2*tile_line_number);
-    ReadTileLine(tile_line, tile_line_address, indentity_palette);
-    for (x = x_screen, tile_column_number = 0; x < (x_screen + 8); x++, tile_column_number++) {
-      if (x < 0 || x >= SCREEN_X_SIZE) {
-        continue;
-      }
-      if (tile_line[tile_column_number]) {
-        screen[(SCREEN_X_SIZE * y) + x] =  apply_palette(tile_line[tile_column_number], palette);
-      }
-    }
-  }
-}
-
-void PPU::DrawSprites() {
-  int x_screen, y_screen;
-  std::bitset<8> object_flag;
-  Byte tile_number; 
-  Byte obp;
-  for (int sprite_location = OAM_START_LOCATION; sprite_location <= OAM_END_LOCATION; sprite_location += 4) {
-    y_screen = mem->GetInAddr(sprite_location) - 16;
-    x_screen = mem->GetInAddr(sprite_location + 1) - 8;
-    tile_number = mem->GetInAddr(sprite_location + 2);
-    object_flag = std::bitset<8>(mem->GetInAddr(sprite_location + 3));
-    if (object_flag[4]) {
-      obp = mem->GetInAddr(rOBP1);
-    } else {
-      obp = mem->GetInAddr(rOBP0);
-    }
-    DrawSingleSprite(y_screen, x_screen, tile_number, obp);
-  }
-}
-
-void PPU::UpdateScreenFromBackground() {
-  int x;
-  int y;
-  int x_deslocated;
-  int y_deslocated;
   const int scx = mem->GetInAddr(rSCX);
-  const int scy = mem->GetInAddr(rSCY);
-  for (y = 0, y_deslocated = scy; y < SCREEN_Y_SIZE; y++) {
-    for (x = 0, x_deslocated = scx; x < SCREEN_X_SIZE; x++) {
-      screen[(SCREEN_X_SIZE * y) + x] = imageData[(BACKGROUND_X_SIZE * y_deslocated) + x_deslocated];
-      if (x_deslocated >= (BACKGROUND_X_SIZE - 1)) {
-        x_deslocated = 0;
-      } else {
-        x_deslocated++;
-      }
-    }
-    if (y_deslocated >= (BACKGROUND_Y_SIZE - 1)) {
-      y_deslocated = 0;
+  int x_deslocated = scx;
+  for (int i = 0; i < SCREEN_X_SIZE; i++) {
+    arr_to_store_line[i] = background_line[x_deslocated];
+    if (x_deslocated >= (BACKGROUND_X_SIZE - 1)) {
+      x_deslocated = 0;
     } else {
-      y_deslocated++;
+      x_deslocated++;
     }
   }
+}
+
+Address* PPU::OAMScan(int Y_cordinate) {
+  Address* sprites = new Address[10]{0};
+  int y_screen;
+  int next_sprite_to_add = 0;
+  for (Address sprite_location = OAM_START_LOCATION; sprite_location < OAM_END_LOCATION; sprite_location += 4) {
+    y_screen = mem->GetInAddr(sprite_location) - 16;
+    if ((Y_cordinate >= y_screen) && (Y_cordinate < (y_screen + 8))) {
+      sprites[next_sprite_to_add] = sprite_location;
+      next_sprite_to_add++;
+    }
+  }
+  return sprites;
+}
+
+void PPU::DrawSpriteLine(ColorNumber* arr_to_store_line, Address sprite_location, int Y_cordinate) {
+  Byte obp;
+  int y_screen = mem->GetInAddr(sprite_location) - 16;
+  int x_screen = mem->GetInAddr(sprite_location + 1) - 8;
+  Byte tile_number = mem->GetInAddr(sprite_location + 2);
+  std::bitset<8> object_flag = std::bitset<8>(mem->GetInAddr(sprite_location + 3));
+  int line_number = Y_cordinate - y_screen;
+  if (object_flag[4]) {
+    obp = mem->GetInAddr(rOBP1);
+  } else {
+    obp = mem->GetInAddr(rOBP0);
+  }
+  ReadTileLine(arr_to_store_line + x_screen, 0x8000 + (16 * tile_number) + (2 * line_number), obp);
+}
+
+void PPU::ScanLine(ColorNumber* arr_to_store_line, int background_Y_line, Byte palette) {
+  // MODE 2
+  Address* sprites = OAMScan(background_Y_line);
+
+  // MODE 3
+  const int scy = mem->GetInAddr(rSCY);
+  background_Y_line = background_Y_line + scy;
+  if (background_Y_line >= (BACKGROUND_Y_SIZE)) {
+    background_Y_line -= BACKGROUND_Y_SIZE;
+  }
+  ScanLineBackground(arr_to_store_line, background_Y_line, palette);
+
+  for (int i = 0; i < 10; i++) {
+    if (sprites[i] != 0) {
+      std::cout << "Drawing sprite: " << i << " | " << (unsigned int) sprites[i]  << std::endl;
+      DrawSpriteLine(arr_to_store_line, sprites[i], background_Y_line);
+    }
+  }
+
+  delete[] sprites;
 }
 
 void PPU::UpdateImageData() {
   Byte palette = mem->GetInAddr(rBGP);
-  ColorNumber* image_data_pt = &(imageData[0]);
-  for (int current_line = 0; current_line < 256; current_line++) {
-    ScanLine(image_data_pt + (256 * current_line), current_line, palette);
+  ColorNumber* image_data_pt = &(screen[0]);
+  for (int LY = 0; LY < SCREEN_Y_SIZE; LY++) {
+    ScanLine(image_data_pt + (SCREEN_X_SIZE * LY), LY, palette);
   }
-  UpdateScreenFromBackground();
-  DrawSprites();
+
 }
 
 }
