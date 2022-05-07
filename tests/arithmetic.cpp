29bb9a8b29720636c21d1645e67e94dee21ecb3c
Raul <raulrufino0@gmail.com>
2022-05-07 09:06:08 -0300

    Move `XOR`, `OR` and `AND` instructions to arithmetic suit

--- tests/arithmetic.cpp
+++ tests/arithmetic.cpp
@@ -40,3 +40,129 @@ TEST(ArithmeticInstructions, XOR_A_C_instruction_zero_flag) {
   EXPECT_EQ(utils::carry_flag(&game.cpu.reg.F), false);
   EXPECT_EQ(game.cpu.reg.PC, 0x101);
 }
+
+TEST(ArithmeticInstructions, XOR_A_A_instruction) {
+  gameboy::Console game;
+  game.initialize_registers();
+  game.mem.SetInAddr(0x100, XOR_A_A);
+  game.cpu.reg.A = 0b00000100;
+
+  game.cpu.execute_intruction(&game.mem);
+
+  EXPECT_EQ(game.cpu.reg.A, 0x0);
+  EXPECT_EQ(game.cpu.reg.F, 0b10000000);
+  EXPECT_EQ(game.cpu.reg.PC, 0x101);
+}
+
+TEST(ArithmeticInstructions, OR_A_C_instruction) {
+  gameboy::Console game;
+  game.initialize_registers();
+  game.mem.SetInAddr(0x100, OR_A_C);
+  game.cpu.reg.A = 0b00000100;
+  game.cpu.reg.C = 0b01000101;
+  game.cpu.reg.F = 0x0;
+
+  game.cpu.execute_intruction(&game.mem);
+
+  EXPECT_EQ(game.cpu.reg.A, 0b01000101);
+  EXPECT_EQ(game.cpu.reg.F, 0x0);
+  EXPECT_EQ(game.cpu.reg.PC, 0x101);
+}
+
+TEST(ArithmeticInstructions, OR_A_C_instruction_affect_zero_flag) {
+  gameboy::Console game;
+  game.initialize_registers();
+  game.mem.SetInAddr(0x100, OR_A_C);
+  game.cpu.reg.A = 0x0;
+  game.cpu.reg.C = 0x0;
+  game.cpu.reg.F = 0x0;
+
+  game.cpu.execute_intruction(&game.mem);
+
+  EXPECT_EQ(game.cpu.reg.A, 0x0);
+  EXPECT_EQ(utils::zero_flag(&game.cpu.reg.F), true);
+  EXPECT_EQ(game.cpu.reg.PC, 0x101);
+}
+
+TEST(ArithmeticInstructions, OR_A_B_instruction) {
+  gameboy::Console game;
+  game.initialize_registers();
+  game.mem.SetInAddr(0x100, OR_A_B);
+  game.cpu.reg.A = 0b00000100;
+  game.cpu.reg.B = 0b01000101;
+  game.cpu.reg.F = 0x0;
+
+  game.cpu.execute_intruction(&game.mem);
+
+  EXPECT_EQ(game.cpu.reg.A, 0b01000101);
+  EXPECT_EQ(game.cpu.reg.F, 0x0);
+  EXPECT_EQ(game.cpu.reg.PC, 0x101);
+}
+
+TEST(ArithmeticInstructions, OR_A_B_instruction_affect_zero_flag) {
+  gameboy::Console game;
+  game.initialize_registers();
+  game.mem.SetInAddr(0x100, OR_A_B);
+  game.cpu.reg.A = 0x0;
+  game.cpu.reg.B = 0x0;
+  game.cpu.reg.F = 0x0;
+
+  game.cpu.execute_intruction(&game.mem);
+
+  EXPECT_EQ(game.cpu.reg.A, 0x0);
+  EXPECT_EQ(utils::zero_flag(&game.cpu.reg.F), true);
+  EXPECT_EQ(game.cpu.reg.PC, 0x101);
+}
+
+TEST(ArithmeticInstructions, AND_A_A_instruction) {
+  gameboy::Console game;
+  game.initialize_registers();
+  game.mem.SetInAddr(0x100, AND_A_A);
+  game.cpu.reg.A = 0b00000100;
+  game.cpu.reg.F = 0x0;
+
+  game.cpu.execute_intruction(&game.mem);
+
+  EXPECT_EQ(game.cpu.reg.A, 0b00000100);
+  EXPECT_EQ(utils::zero_flag(&game.cpu.reg.F), false);
+  EXPECT_EQ(utils::subtract_flag(&game.cpu.reg.F), false);
+  EXPECT_EQ(utils::half_carry_flag(&game.cpu.reg.F), true);
+  EXPECT_EQ(utils::carry_flag(&game.cpu.reg.F), false);
+  EXPECT_EQ(game.cpu.reg.PC, 0x101);
+}
+
+TEST(ArithmeticInstructions, AND_A_d8_instruction) {
+  gameboy::Console game;
+  game.initialize_registers();
+  game.mem.SetInAddr(0x100, AND_A_d8);
+  game.mem.SetInAddr(0x101, 0b10111010);
+  game.cpu.reg.A = 0b00100111;
+  game.cpu.reg.F = 0x0;
+
+  game.cpu.execute_intruction(&game.mem);
+
+  EXPECT_EQ(game.cpu.reg.A, 0b00100010);
+  EXPECT_EQ(utils::zero_flag(&game.cpu.reg.F), false);
+  EXPECT_EQ(utils::subtract_flag(&game.cpu.reg.F), false);
+  EXPECT_EQ(utils::half_carry_flag(&game.cpu.reg.F), true);
+  EXPECT_EQ(utils::carry_flag(&game.cpu.reg.F), false);
+  EXPECT_EQ(game.cpu.reg.PC, 0x102);
+}
+
+TEST(ArithmeticInstructions, AND_A_d8_instruction_zero_flag) {
+  gameboy::Console game;
+  game.initialize_registers();
+  game.mem.SetInAddr(0x100, AND_A_d8);
+  game.mem.SetInAddr(0x101, 0b11110000);
+  game.cpu.reg.A = 0b00001111;
+  game.cpu.reg.F = 0x0;
+
+  game.cpu.execute_intruction(&game.mem);
+
+  EXPECT_EQ(game.cpu.reg.A, 0);
+  EXPECT_EQ(utils::zero_flag(&game.cpu.reg.F), true);
+  EXPECT_EQ(utils::subtract_flag(&game.cpu.reg.F), false);
+  EXPECT_EQ(utils::half_carry_flag(&game.cpu.reg.F), true);
+  EXPECT_EQ(utils::carry_flag(&game.cpu.reg.F), false);
+  EXPECT_EQ(game.cpu.reg.PC, 0x102);
+}
